217ef84c315d12126bdc778f77a2aa98339edcc7
fbdthree <112531356+fbdthree@users.noreply.github.com>
2023-03-06 11:34:48 -0500

    mvp

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
-  const tx = jadeDb.transaction('jate', 'readWrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({id:1,value:content})
   const result = await request;
@@ -30,7 +30,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jadeDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
